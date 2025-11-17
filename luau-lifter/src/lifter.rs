72800ff2e19a44e8b0341715897a9a407bac73af
Horizon <244117393+horizzondev@users.noreply.github.com>
2025-11-17 16:40:44 -0500

    Revert "Added panic handling for unimplemented stuff"
    This reverts commit 5bf265b65eae73d23e5e9e482809474408d79259.

--- luau-lifter/src/lifter.rs
+++ luau-lifter/src/lifter.rs
@@ -1339,17 +1339,10 @@ impl<'a> Lifter<'a> {
                         }
                         _ => unreachable!("{:?}", instruction),
                     }
-                instruction => {
-                    eprintln!(
-                        "Unimplemented instruction at PC {}: {:?}",
-                        block_start + index,
-                        instruction
-                    );
-                    eprintln!("Block range: {}..{}", block_start, block_end);
-                    unimplemented!("{:?}", instruction);
-                }
+                _ => unimplemented!("{:?}", instruction),
             }
         }
+
         let last_index = iter
             .next()
             .map(|(i, _)| block_start + i - 1)
